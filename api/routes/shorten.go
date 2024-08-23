58f04581588c267c79e23b544a250f4cd8f48e8f
Divyanshi_Singh_Bora <bora.1@iitj.ac.in>
2024-08-24 02:41:14 +0530

    update 2.0

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,8 +1,12 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/thesilentline/shorten-url-project/database"
 	"github.com/thesilentline/shorten-url-project/helpers"
 )
 
@@ -23,10 +27,32 @@ type response struct {
 
 func ShortenURL(c *fiber.Ctx) error {
 
-	body := new(request)	//body now points to a newly created request struct in memory
+	// body now points to a newly created request struct in memory
+	body := new(request)	
 
 	if err := c.BodyParser(&body); err!= nil{
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"cannot parse json"},)
+	}
+
+	// impleament rate limiting
+	r2 := database.CreateClient(1)	// redis database client
+	defer r2.Close()
+
+	val, err := r2.GET(database.Ctx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.SET(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		val, _ = r2.GET(database.Ctx, c.IP()).Result()
+		valInt, _ := strconv.Atoi(val)
+
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error": "rate limit exceeded",
+				"rate limit reset": limit / time.Nanosecond / time.Minute,
+			})
+
+		}
 	}
 
 	// check if the url entered is correct
@@ -41,5 +67,8 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	//enforce HTTP SSL
 	body.URL = helpers.EnforceHTTP(body.URL)
+
+	// decrement the counter
+	r2.Decr(database.Ctx, c.IP())
 
 }
