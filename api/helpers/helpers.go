0e7a8eeba5a227801ec7f5048305cda2bfef6050
Divyanshi_Singh_Bora <bora.1@iitj.ac.in>
2024-08-23 14:46:12 +0530

    update 2

--- api/helpers/helpers.go
+++ api/helpers/helpers.go
@@ -1,5 +1,7 @@
 package helpers
 
+import "os"
+
 func EnforceHTTP(url string) string {
 
 	if url[:4] != "http"{
@@ -11,5 +13,18 @@ func EnforceHTTP(url string) string {
 
 func RemoveDomainError(url string) bool {
 	 
+	if url == os.Getenv("DOMAIN"){
+		return false
+	}
 
+	newURL := strings.Replace(url,"http://","",1)
+	newURL = strings.Replace(newURL,"https://","",1)
+	newURL = strings.Replace(newURL,"www.","",1)
+	newURL = strings.Split(newURL,"/")[0]
+
+	if newURL == os.Getenv("DOMAIN"){
+		return false
+	}
+
+	return true
 }
