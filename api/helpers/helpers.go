43095123fe5c94c4e1feef0d164b3312faba052a
Divyanshi_Singh_Bora <bora.1@iitj.ac.in>
2024-08-23 13:14:00 +0530

    update 1.0

---
+++ api/helpers/helpers.go
@@ -0,0 +1,15 @@
+package helpers
+
+func EnforceHTTP(url string) string {
+
+	if url[:4] != "http"{
+		return "http://" + url
+	}
+
+	return url
+}
+
+func RemoveDomainError(url string) bool {
+	 
+
+}
